0a939958116519158a31977a990784a0eab5d609
Ali-Aref1 <ali14915@gmail.com>
2025-05-14 01:35:10 +0300

    added gitignore + commented unused import for file that doesn't exist YA KAREEM

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Member } from '../components/HighBoard'
 import { Heads } from '../components/Heads'
-import carbg from "../assets/images/car-bg.png"
+//import carbg from "../assets/images/car-bg.png" This image isn't used and the import should be removed
 
 export const TeamPage = () => {
   return (
